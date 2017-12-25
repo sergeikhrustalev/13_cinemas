3c874dbf9b4ae9e870ec8b9a92d888ac25873ac8
Sergei Khrustalev <sergei@khrustalev.net>
2017-12-25 17:07:37 +0300

    first commit

--- cinemas.py
+++ cinemas.py
@@ -1,11 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
 
 
 def fetch_afisha_page():
-    pass
+    
+    afisha_page = 'https://www.afisha.ru/msk/schedule_cinema/'    
+    
+    return requests.get(afisha_page).content
 
 
 def parse_afisha_list(raw_html):
-    pass
+    
+    soup = BeautifulSoup(html, 'html.parser')
+    
+    return [
+        
+        dict(
+            movie_title=movie.h3.a.string,
+            cinema_count=len(movie.find_all('td', class_='b-td-item')),
+        )
+        
+        for movie in soup.find_all('div', class_='s-votes-hover-area')
+        
+    ]
 
 
 def fetch_movie_info(movie_title):
@@ -17,4 +34,13 @@ def output_movies_to_console(movies):
 
 
 if __name__ == '__main__':
-    pass
+    
+    
+    html = fetch_afisha_page()
+    
+    print(parse_afisha_list(html))
+    
+    
+    
+    
+    
