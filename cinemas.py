415f82514a040ae6d0c89ff3d0884de9c81c005c
Sergei Khrustalev <sergei@khrustalev.net>
2017-12-26 13:04:30 +0300

    first demo release

--- cinemas.py
+++ cinemas.py
@@ -9,15 +9,15 @@ def fetch_afisha_page():
     ).text
 
 
-def parse_afisha_list(raw_html):
+def parse_afisha_list(html):
 
-    soup = BeautifulSoup(raw_html, 'html.parser')
+    soup = BeautifulSoup(html, 'html.parser')
 
     return [
 
         dict(
-            movie_title=movie.h3.a.string,
-            cinema_count=len(movie.find_all('td', class_='b-td-item')),
+            title=movie.h3.a.string,
+            cinemas=len(movie.find_all('td', class_='b-td-item')),
         )
 
         for movie in soup.find_all('div', class_='s-votes-hover-area')
@@ -25,37 +25,27 @@ def parse_afisha_list(raw_html):
     ]
 
 
-def fetch_movie_info(movie_title):
+def fetch_movie_info(title):
 
     headers = {
-        'Accept-Language': 'ru',
-        'Host': 'www.kinopoisk.ru',
-        'Referer': 'https://www.kinopoisk.ru/',
+
         'User-Agent': 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0'
     }
 
-    movie_page = requests.get(
-        'https://www.kinopoisk.ru/index.php',
+    rate_info = requests.get(
+        'https://www.kinopoisk.ru/search/suggest/',
         headers=headers,
-        params=dict(kp_query=movie_title),
-        timeout=10
-    ).text
-
-    soup = BeautifulSoup(movie_page, 'html.parser')
+        params=dict(value=title),
+    ).json()['page']['suggest']['items']['movies'][0]['ratings']['kp']
 
     return dict(
 
-        movie_rating=soup.find(
-            'span',
-            class_='movie-snippet__rating-value'
-        ).string,
+        rating=float(rate_info['value']) if rate_info['isReady'] else 0.0,
 
-        votes_count=soup.find(
-            'span',
-            class_='movie-snippet__rating-votes'
-        ).string,
-
+        votes=str(rate_info['count'])
+        
     )
+
 
 def output_movies_to_console(movies):
     pass
@@ -69,13 +59,18 @@ if __name__ == '__main__':
         
         dict(
             
-            list(afisha_item.items()) + list(fetch_movie_info(afisha_item['movie_title']).items())
+            list(
+                afisha_item.items()
+            ) + 
+            
+            list(
+                fetch_movie_info(afisha_item['title']).items()
+            )
         )
 
         for afisha_item in parse_afisha_list(html)
-        if afisha_item['cinema_count'] > 10
+        if afisha_item['cinemas'] > 20
 
     ]
 
     print(total)
-
