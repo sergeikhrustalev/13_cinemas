2d25c9a78e3e734575e7e4d22fc31b1046cad6ec
sergei <sergei@Mac-mini-sergei.local>
2017-12-26 14:19:32 +0300

    add prepare data function

--- cinemas.py
+++ cinemas.py
@@ -28,9 +28,10 @@ def parse_afisha_list(html):
 def fetch_movie_info(title):
 
     headers = {
-
         'User-Agent': 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0'
     }
+    
+    print(title)
 
     rate_info = requests.get(
         'https://www.kinopoisk.ru/search/suggest/',
@@ -39,38 +40,40 @@ def fetch_movie_info(title):
     ).json()['page']['suggest']['items']['movies'][0]['ratings']['kp']
 
     return dict(
-
         rating=float(rate_info['value']) if rate_info['isReady'] else 0.0,
-
         votes=str(rate_info['count'])
-        
     )
 
 
-def output_movies_to_console(movies):
-    pass
+def prepare_movies_data():
+
+    return sorted(
+
+        [
+
+            dict(
+
+                list(
+                    afisha_item.items()
+                ) +
+
+                list(
+                    fetch_movie_info(afisha_item['title']).items()
+                )
+
+            )
+
+            for afisha_item in parse_afisha_list(fetch_afisha_page())
+            if afisha_item['cinemas'] > 5
+
+        ],
+
+        key=lambda k: k['rating'],
+
+        reverse=True,
+
+    )
 
 
 if __name__ == '__main__':
-
-    html = fetch_afisha_page()
-
-    total = [
-        
-        dict(
-            
-            list(
-                afisha_item.items()
-            ) + 
-            
-            list(
-                fetch_movie_info(afisha_item['title']).items()
-            )
-        )
-
-        for afisha_item in parse_afisha_list(html)
-        if afisha_item['cinemas'] > 20
-
-    ]
-
-    print(total)
+    print(prepare_movies_data())
