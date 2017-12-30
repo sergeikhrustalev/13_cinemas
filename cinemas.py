12afb2a30f8a1874223af6df7b3433817aea4521
sergei <sergei@Mac-mini-sergei.local>
2017-12-31 01:55:50 +0300

    code refactoring

--- cinemas.py
+++ cinemas.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 
 
 def fetch_response(url, params=None):
-
     headers = {
         'User-Agent': 'Mozilla/5.0 (Mobile; rv:15.0) Gecko/15.0 Firefox/15.0'
     }
@@ -16,9 +15,7 @@ def fetch_response(url, params=None):
 
 
 def parse_afisha_info(html_page):
-
     soup = BeautifulSoup(html_page, 'html.parser')
-
     return [
 
         dict(
@@ -27,84 +24,68 @@ def parse_afisha_info(html_page):
         )
 
         for movie in soup.find_all('div', class_='s-votes-hover-area')
-
     ]
 
 
 def parse_kinopoisk_info(json_content):
-
-    rate_info = json_content['page']['suggest']['items']['movies'][0]['ratings']
-
-    if rate_info is None:
+    rate = json_content['page']['suggest']['items']['movies'][0]['ratings']
+    if rate is None:
         return dict(rating=0.0, votes='0')
-
     return dict(
 
         rating=(
-            float(rate_info['kp']['value'])
-            if rate_info['kp']['isReady'] else 0.0
+            float(rate['kp']['value'])
+            if rate['kp']['isReady'] else 0.0
         ),
 
         votes=str(rate_info['kp']['count'])
-
     )
 
 
 def prepare_movies(min_cinema_count=10):
-
-    afisha_page = fetch_response('https://www.afisha.ru/msk/schedule_cinema/').text
+    afisha_page = fetch_response(
+        'https://www.afisha.ru/msk/schedule_cinema/'
+    ).text
 
     movies = list()
-
     for afisha_item in parse_afisha_info(afisha_page):
         if afisha_item['cinemas'] >= min_cinema_count:
 
-            kinopoisk_json = fetch_response( 'https://www.kinopoisk.ru/search/suggest/', params=dict(value=afisha_item['title'])).json()
+            kinopoisk_json = fetch_response(
+                'https://www.kinopoisk.ru/search/suggest/',
+                params=dict(value=afisha_item['title'])
+            ).json()
+
             kinopoisk_item = parse_kinopoisk_info(kinopoisk_json)
 
             movies.append(
                 dict(**afisha_item, **kinopoisk_item)
             )
 
-    return sorted(
-        
-        movies,
-
-        key=lambda k: k['rating'],
-
-        reverse=True,
-
-    )
+    return sorted(movies, key=lambda k: k['rating'], reverse=True)
 
 
 def output_table_to_console(table, rows_count=10):
-
     print(
-
         '{:50}{:6} {:>10}{:>10}'.format(
             'TITLE',
             'RATING',
             'VOTES',
             'CINEMAS',
         )
-
     )
 
     for row in table[:rows_count]:
-
         print(
-
             '{:50}{:6} {:>10}{:>10}'.format(
                 row['title'],
                 row['rating'],
                 row['votes'],
                 row['cinemas'],
             )
-
         )
 
 
 if __name__ == '__main__':
-
     movies = prepare_movies()
     output_table_to_console(table=movies)
